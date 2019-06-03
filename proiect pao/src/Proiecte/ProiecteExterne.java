9826e3c4da13ef44b3e544c9f7d70b91add9711e
marianeferu <48251558+marianeferu@users.noreply.github.com>
2019-06-03 19:09:16 +0300

    Update and rename Proiecte_Externe.java to ProiecteExterne.java

--- proiect pao/src/Proiecte/ProiecteExterne.java
+++ proiect pao/src/Proiecte/ProiecteExterne.java
@@ -1,12 +1,12 @@
 package Proiecte;
 
-public class Proiecte_Externe extends  Proiecte{
+public class ProiecteExterne extends  Proiecte{
 
 
     private String locatie;
     private Integer cheltuieli;   ///transport
 
-    public Proiecte_Externe(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, String locatie, Integer cheltuieli) {
+    public ProiecteExterne(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, String locatie, Integer cheltuieli) {
         super(data, suma_alocata, nr_participanti, id_coordonator, denumire);
         this.locatie = locatie;
         this.cheltuieli = cheltuieli;
