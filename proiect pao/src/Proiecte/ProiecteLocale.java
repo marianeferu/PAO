75ee4eed35801a50e57d89e32b8994fb1c2988cd
marianeferu <48251558+marianeferu@users.noreply.github.com>
2019-06-03 19:09:58 +0300

    Update and rename Proiecte_Locale.java to ProiecteLocale.java

--- proiect pao/src/Proiecte/ProiecteLocale.java
+++ proiect pao/src/Proiecte/ProiecteLocale.java
@@ -1,11 +1,11 @@
 package Proiecte;
 
-public class Proiecte_Locale extends Proiecte {
+public class ProiecteLocale extends Proiecte {
 
     private Evenimente evenimente;
 
 
-    public Proiecte_Locale(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, Evenimente ev) {
+    public ProiecteLocale(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, Evenimente ev) {
         super(data, suma_alocata, nr_participanti, id_coordonator, denumire);
 
         evenimente = new Evenimente(ev);
