8f665cd744acf0ba07a99495d10fb3127fe1c545
marianeferu <48251558+marianeferu@users.noreply.github.com>
2019-06-03 19:26:35 +0300

    Update ProiecteLocale.java

--- proiect pao/src/Proiecte/ProiecteLocale.java
+++ proiect pao/src/Proiecte/ProiecteLocale.java
@@ -1,15 +1,23 @@
 package Proiecte;
 
 public class ProiecteLocale extends Proiecte {
+    
+    private Integer nr_voluntari;
 
-    private Evenimente evenimente;
-
-
-    public ProiecteLocale(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, Evenimente ev) {
+  public Proiecte_Locale(String data, Integer suma_alocata, String nr_participanti, String id_coordonator, String denumire, Integer nr_voluntari, Evenimente ev) {
         super(data, suma_alocata, nr_participanti, id_coordonator, denumire);
+        this.nr_voluntari = nr_voluntari;
 
         evenimente = new Evenimente(ev);
 
 
+    }
+
+    public Integer getNr_voluntari() {
+        return nr_voluntari;
+    }
+
+    public void setNr_voluntari(Integer nr_voluntari) {
+        this.nr_voluntari = nr_voluntari;
     }
 }
